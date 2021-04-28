6e1b9f44a45719cabcf9611af2774b1afe0ed9a6
Cesar <30559667+CesarAroon@users.noreply.github.com>
2021-04-28 13:09:02 -0500

    Add files via upload

---
+++ numeroalCuadrado/numeroCuadrado/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace numeroCuadrado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcularCuadrado = new System.Windows.Forms.Button();
+            this.TXTnumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcularCuadrado
+            // 
+            this.btnCalcularCuadrado.Location = new System.Drawing.Point(266, 132);
+            this.btnCalcularCuadrado.Name = "btnCalcularCuadrado";
+            this.btnCalcularCuadrado.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcularCuadrado.TabIndex = 0;
+            this.btnCalcularCuadrado.Text = "CALCULAR EL CUADRADO";
+            this.btnCalcularCuadrado.UseVisualStyleBackColor = true;
+            this.btnCalcularCuadrado.Click += new System.EventHandler(this.btnCalcularCuadrado_Click);
+            // 
+            // TXTnumero
+            // 
+            this.TXTnumero.Location = new System.Drawing.Point(266, 67);
+            this.TXTnumero.Name = "TXTnumero";
+            this.TXTnumero.Size = new System.Drawing.Size(100, 23);
+            this.TXTnumero.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INGRESA UN NUMERO";
+            // 
+            // textResultado
+            // 
+            this.textResultado.Location = new System.Drawing.Point(266, 188);
+            this.textResultado.Name = "textResultado";
+            this.textResultado.Size = new System.Drawing.Size(100, 23);
+            this.textResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXTnumero);
+            this.Controls.Add(this.btnCalcularCuadrado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcularCuadrado;
+        private System.Windows.Forms.TextBox TXTnumero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textResultado;
+    }
+}
+
